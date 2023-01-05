979e01ffcd5391a29e229ec7fc6f1166a3d7e26c
He Haoyu <73004524+yuhehao@users.noreply.github.com>
2023-01-05 10:14:35 +0800

    push a warning in comment

--- init.py
+++ init.py
@@ -19,26 +19,6 @@ with open("data/test_data.pickle", "wb") as f:
 # use following code replace the above code 
 # spark = SparkSession.builder.appName('Read CSV File into DataFrame').getOrCreate()
 
-# _test_data_spark = spark.read.csv('data/test.csv', sep='\t')
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c0", "Row")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c1", "Anon Student Id")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c2", "Problem Hierarchy")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c3", "Problem Name")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c4", "Problem View")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c5", "Step Name")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c6", "Step Start Time")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c7", "First Transaction Time")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c8", "Correct Transaction Time")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c9", "Step End Time")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c10", "Step Duration (sec)")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c11", "Correct Step Duration (sec)")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c12", "Error Step Duration (sec)")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c13", "Correct First Attempt")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c14", "Incorrects")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c15", "Hints")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c16", "Corrects")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c17", "KC(Default)")
-# _test_data_spark = _test_data_spark.withColumnRenamed("_c18", "Opportunity(Default)")
 
 # _train_data_spark = spark.read.csv('data/train.csv', sep='\t')
 # _train_data_spark = _train_data_spark.withColumnRenamed("_c0", "Row")
@@ -63,6 +43,30 @@ with open("data/test_data.pickle", "wb") as f:
 
 # train_data = _train_data_spark.toPandas()
 # train_data = train_data.drop(index=[0])
+
+# this have some complex problem and use below code is Ok, but test data is loading by pd.csv_read better!!!!!!!!!!
+
+# _test_data_spark = spark.read.csv('data/test.csv', sep='\t')
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c0", "Row")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c1", "Anon Student Id")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c2", "Problem Hierarchy")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c3", "Problem Name")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c4", "Problem View")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c5", "Step Name")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c6", "Step Start Time")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c7", "First Transaction Time")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c8", "Correct Transaction Time")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c9", "Step End Time")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c10", "Step Duration (sec)")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c11", "Correct Step Duration (sec)")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c12", "Error Step Duration (sec)")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c13", "Correct First Attempt")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c14", "Incorrects")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c15", "Hints")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c16", "Corrects")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c17", "KC(Default)")
+# _test_data_spark = _test_data_spark.withColumnRenamed("_c18", "Opportunity(Default)")
+
 # test_data = _test_data_spark.toPandas()
 # test_data = test_data.drop(index=[0])
 
