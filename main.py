516d232c9b17beb458dae179d25c903342092316
He Haoyu <73004524+yuhehao@users.noreply.github.com>
2023-01-05 18:32:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ with open("data/train_data.pickle", "rb") as f:
     train_data = pickle.load(f)
 with open("data/test_data.pickle", "rb") as f:
     test_data = pickle.load(f)
-with open("data/data.pickle", "rb") as f:
-    data = pickle.load(f)
+# with open("data/data.pickle", "rb") as f:
+#     data = pickle.load(f)
 
 with open("data/aggdf.pickle", "rb") as f:
     aggdf = pickle.load(f)
@@ -34,25 +34,27 @@ print("data has been ready!")
 # ML
 # logistic regression
 from sklearn.linear_model import LogisticRegression
-lr_model = LogisticRegression(solver="liblinear", n_jobs=-16, max_iter=1000) # Enable all CPUs
+lr_model = LogisticRegression(solver="liblinear", n_jobs=-16, max_iter=2000) # Enable all CPUs
 lr_model.fit(X, Y)
 print(type(lr_model))
+
+# random forest
+from sklearn import ensemble
+est_count = 1000
+rf_model = ensemble.RandomForestClassifier(n_estimators = est_count, criterion="entropy", max_depth=30, n_jobs=-16) # Enable all CPUs
+rf_model.fit(X, Y)
+
+
+
+# using pyspark
 # from pyspark.ml.classification import LogisticRegression
 # lr = LogisticRegression(labelCol="Correct First Attempt", maxIter=10).fit(X)
 # lr_pred = lr.transform(Y)
 # print(lr_pred.show())
 # print("len:", len(lr_pred))
-# random forest
-# from sklearn import ensemble
-# est_count = 100
-# rf_model = ensemble.RandomForestClassifier(n_estimators = est_count, criterion="entropy", max_depth=10, n_jobs=-16) # Enable all CPUs
-# rf_model.fit(X, Y)
-
-
-# using pyspark
-from pyspark.sql import SparkSession
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
+# from pyspark.sql import SparkSession
+# from pyspark import SparkContext
+# from pyspark.sql import SQLContext
 # spark=SparkSession.builder.appName('data_processing').getOrCreate()
 # sc = SparkContext()
 # sqlContest = SQLContext(sc)
@@ -80,24 +82,22 @@ def RMSE(P, Y):
 P = lr_model.predict_proba(X)[:, 1]
 P_ = lr_model.predict_proba(X_)[:, 1]
 
-print("lenP_:",len(P_))
-print("lenY_:",len(Y_))
+print("logistic regression Train Error:", RMSE(P, Y))
+print("logistic regression Test Error:", RMSE(P_, Y_))
 
-print("Train Error:", RMSE(P, Y))
-print("Test Error:", RMSE(P_, Y_))
 # Generate submission
-# RES = P_[np.isnan(Y_)]
-# test_data.loc[np.isnan(test_data["Correct First Attempt"]), "Correct First Attempt"] = RES
-# test_data.to_csv("data/predict_result.csv", sep='\t', index=False)
+RES = P_[np.isnan(Y_)]
+test_data.loc[np.isnan(test_data["Correct First Attempt"]), "Correct First Attempt"] = RES
+test_data.to_csv("data/predict_result.csv", sep='\t', index=False)
 
-# # Save
-# pd.Series(P).to_csv("data/sparse_train.csv", sep='\t', header=["sparse_res"])
-# pd.Series(P_).to_csv("data/sparse_test.csv", sep='\t', header=["sparse_res"])
+# Save
+pd.Series(P).to_csv("data/sparse_train.csv", sep='\t', header=["sparse_res"])
+pd.Series(P_).to_csv("data/sparse_test.csv", sep='\t', header=["sparse_res"])
 
-# # Random Forest
+# Random Forest
 
-# P = rf_model.predict_proba(X)[:, 1]
-# P_ = rf_model.predict_proba(X_)[:, 1]
+P = rf_model.predict_proba(X)[:, 1]
+P_ = rf_model.predict_proba(X_)[:, 1]
 
-# print("Train Error:", RMSE(P, Y))
-# print("Test Error:", RMSE(P_, Y_))
+print("Random Forest Train Error:", RMSE(P, Y))
+print("Random Forest Test Error:", RMSE(P_, Y_))
